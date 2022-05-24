17ab255c41d138fce46d80fe5cbbd7731a84ad95
Giuseppe222 <ciocpep@gmail.com>
2022-05-24 15:35:20 +0200

    ho aggiornato Giocatore

---
+++ src/model/Giocatore.java
@@ -0,0 +1,46 @@
+public class Giocatore extends Utente{
+
+    public Giocatore(String email){
+        super(email);
+    }
+
+    public Integer getPunteggio(){
+
+    }
+
+    public Set<Professore> getProfessoriSeguiti(){
+
+    }
+
+    public void setProfessoriSeguiti(Set<Professore> ProfessoriSeguiti){
+
+    }
+
+    public Set<Professori> getCommissione(){
+
+    }
+
+    public void setCommissione(Set<Professori> Commissione){
+
+    }
+
+    public CorsoDiLaurea getCorsoDiLaurea(){
+
+    }
+
+    public void setCorsoDiLaurea(CorsoDiLaurea corsoDiLaurea){
+
+    }
+
+    public Set<Classifica> getClassifichePrivate(){
+
+    }
+
+    public void partecipaClassifica(Classifica classifica){
+
+    }
+
+    public void abbandonaClassifica(Classifica classifica){
+        
+    }
+}
