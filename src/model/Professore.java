4b41dcf10143ed048c6261c8a041933014fb8f5d
Giuseppe222 <ciocpep@gmail.com>
2022-05-24 15:32:37 +0200

    ho aggiunto le basi per qualche bean

---
+++ src/model/Professore.java
@@ -0,0 +1,53 @@
+public class Professore{
+    
+    private String nome;
+    private String cognome;
+    private String email;
+
+    public Professore(String nome, String cognome, String email){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCognome(){
+        return this.cognome;
+    }
+
+    public void setCognome(String cognome){
+        this.cognome = cognome;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public CorsoDiLaurea getCorsoDiLaurea(){
+
+    }
+
+    public void setCorsiDiLaurea(Set<CorsoDiLaurea> corsiDiLAurea){
+
+    }
+
+    public Set<EventoAvvenuto> getEventiAvvenuti(){
+
+    }
+
+    public void aggiungiEventoAvvenuto(EventoAvvenuto eventoAvvenuto){
+        
+    }
+
+}
