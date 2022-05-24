4b04eab39ae4e9b1cf2d3ce1fdafa0f1953e7630
Giuseppe222 <ciocpep@gmail.com>
2022-05-24 16:57:42 +0200

    Utente pubblicato per Pietro

---
+++ src/beans/Utente.java
@@ -0,0 +1,31 @@
+public class Utente {
+    String email;
+
+    public Utente(String email){
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public Ruolo getRuolo(){
+        
+    }
+
+    public void setRuolo(Ruolo ruolo){
+
+    }
+
+    public void blocca(){
+
+    }
+
+    public boolean isBloccato(){
+
+    }
+}
