56dde5b30c8222cdc65a3576c51bbfc7fa98baec
pietroruffi <ruffinelli23@gmail.com>
2022-05-26 14:59:43 +0200

    classpath test aggiornato

---
+++ test/collaudo/TestCollaudo.java
@@ -0,0 +1,26 @@
+package collaudo;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import beans.*;
+
+public class TestCollaudo {
+ 
+    private VotazioneAzioneSignificativa votazAzioneGlobale;
+    private VotazioneAzioneSignificativa votazAzioneIngInf;
+    private VotazioneEventoAvvenuto votazEvento;
+    private Professore professore;
+    private CorsoDiLaurea ingInf;
+    private AzioneSignificativa azioneSign;
+    private Risposta risposta;
+    private Giocatore giocatore;
+    private LocalDateTime dataOra;
+
+}
