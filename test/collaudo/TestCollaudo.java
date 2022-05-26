386bf97da7c5a69bcdca8027dbd2d5f61280994d
pietroruffi <ruffinelli23@gmail.com>
2022-05-26 17:32:59 +0200

    continuo test

--- test/collaudo/TestCollaudo.java
+++ test/collaudo/TestCollaudo.java
@@ -3,11 +3,14 @@ package collaudo;
 import static org.junit.Assert.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import beans.*;
+import controller.ClassificheController;
 
 public class TestCollaudo {
  
@@ -20,6 +23,14 @@ public class TestCollaudo {
     private Risposta risposta;
     private Giocatore giocatore;
     private LocalDateTime dataOra;
+    private Professore prof1;
+    private Professore prof2;
+    private Professore prof3;
+    private Professore prof4;
+    private Set<Professore> commissione;
+    private Set<Professore> professoriSeguiti;
+    private ClassificaPrivata classifica;
+
 
     @Before
     public void SetUp(){
@@ -32,8 +43,8 @@ public class TestCollaudo {
         // Creo i professori che comporranno commissione e professoriSeguiti del giocatore
         prof1 = new Professore("Pino", "Silvestre", "pino.silvestre@unibo.it");
         prof2 = new Professore("Massimo", "Voltaggio", "massimo.voltaggio@unibo.it");
-        Professore prof3 = new Professore("Bianca", "Neve", "bianca.neve@unibo.it");
-        Professore prof4 = new Professore("Domenica", "D'agosto", "domenica.dagosto@unibo.it");
+        prof3 = new Professore("Bianca", "Neve", "bianca.neve@unibo.it");
+        prof4 = new Professore("Domenica", "D'agosto", "domenica.dagosto@unibo.it");
 
         // Affinche' commissione e professoriSeguiti siano validi, i professori devono insegnare (anche) nel corso del giocatore
         prof1.getCorsiDiLaurea().add(ingInf);
@@ -56,14 +67,26 @@ public class TestCollaudo {
 
         // Imposto i parametri del giocatore
         giocatore.setCorsoDiLaurea(ingInf);
-        
+        giocatore.setCommissione(commissione);
+        giocatore.setProfessoriSeguiti(professoriSeguiti);
+
+        //Creo una nuova classifica privata
+        classifica = new ClassificaPrivata("Una nuova classifica");
 
 
     }
 
     @Test
     public void testClassifiche(){
+        ClassificheController controllerClassifiche = new ClassificheController();
 
+        controllerClassifiche.aggiungiClassifica(classifica, "chiave");
+
+        controllerClassifiche.partecipaClassifica("alfredo.dalcaldo@studio.unibo.it", classifica, "chiave");
+
+        Classifica c = controllerClassifiche.ottieniClassifica(23);
+
+        assertEquals(classifica, c);
     }
 
     @Test
